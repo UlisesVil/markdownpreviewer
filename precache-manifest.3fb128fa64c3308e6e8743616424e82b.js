987a1c6ba6611248f43fb8346a3d17c45fb3cbb5
github_ulisesvil <ulisesvil5@hotmail.com>
2020-07-05 00:36:34 -0500

    Updates

---
+++ precache-manifest.3fb128fa64c3308e6e8743616424e82b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4232032454d0dae01b1e9373d025cb75",
+    "url": "/markdownpreviewer/index.html"
+  },
+  {
+    "revision": "ded56b487a3be08083a3",
+    "url": "/markdownpreviewer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e7f0d8c25c655d642b2c",
+    "url": "/markdownpreviewer/static/js/2.29a9ffad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdownpreviewer/static/js/2.29a9ffad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded56b487a3be08083a3",
+    "url": "/markdownpreviewer/static/js/main.5cb85ecd.chunk.js"
+  },
+  {
+    "revision": "bd7b19a68e29be4357cc",
+    "url": "/markdownpreviewer/static/js/runtime-main.3c35acc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/markdownpreviewer/static/media/logo.5d5d9eef.svg"
+  }
+]);
