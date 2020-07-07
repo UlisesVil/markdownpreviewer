c4982b0e134482871052bbcc366911f3cc499010
github_ulisesvil <ulisesvil5@hotmail.com>
2020-07-06 19:14:02 -0500

    code finish whithout style and marked error solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+
+//importar componentes
+import MarkdownPreviewer from './components/MarkdownPreviewer.js';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="components">
+
+            <MarkdownPreviewer />
+    
     </div>
+    
   );
 }
 
